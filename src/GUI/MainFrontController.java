b73f3bd262671667f465691092a89eec4a7437e8
ahmed missaoui <ahmed.missaoui1@esprit.tn>
2023-04-23 15:28:24 +0200

    shop

--- src/GUI/MainFrontController.java
+++ src/GUI/MainFrontController.java
@@ -4,17 +4,21 @@
  */
 package GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafxpi.JavafxPi;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 /**
  * FXML Controller class
@@ -29,6 +33,16 @@ public class MainFrontController implements Initializable {
     private ImageView progb;
     @FXML
     private AnchorPane panev;
+    @FXML
+    private ImageView shop;
+    
+    
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    
+    
 
     /**
      * Initializes the controller class.
@@ -63,6 +77,15 @@ System.out.println("no file found");
 
     @FXML
     private void progv(MouseEvent event) {
+    }
+
+    @FXML
+    private void shopaction(MouseEvent event) throws IOException {
+   Parent root = FXMLLoader.load(getClass().getResource("Shop.fxml"));
+   stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+   scene = new Scene(root);
+   stage.setScene(scene);
+   stage.show();
     }
     
 }
