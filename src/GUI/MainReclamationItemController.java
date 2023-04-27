ac5fb63b2c52976c5ea8fb0d6043fdc90db6aa00
ahmed missaoui <ahmed.missaoui1@esprit.tn>
2023-04-28 01:37:01 +0200

    pdf reclam

--- src/GUI/MainReclamationItemController.java
+++ src/GUI/MainReclamationItemController.java
@@ -4,6 +4,15 @@
  */
 package GUI;
 
+
+
+
+
+//import org.controlsfx.control.Notifications;
+
+
+
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -48,8 +57,7 @@ public class MainReclamationItemController implements Initializable {
     ReponseService psrep = new ReponseService();
     @FXML
     private AnchorPane pane1;
-    @FXML
-    private ImageView pdf;
+    Reclamation reclam;
 
     /**
      * Initializes the controller class.
@@ -57,13 +65,19 @@ public class MainReclamationItemController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    } 
+    
+    
+    public void  refresh()
+     {
+         vbox.getChildren().removeAll(vbox.getChildren());
+     }
     
      public void setData(Reclamation pan)
     {
        
             id = pan.getId();
-    
+    reclam = pan;
             Image image2;
             
             image2 = new Image("/Images/delete.png");
@@ -90,8 +104,6 @@ public class MainReclamationItemController implements Initializable {
          pane1.getChildren().removeAll(pane1.getChildren());
     }
 
-    @FXML
-    private void pdfaction(MouseEvent event) {
-    }
+ 
     
 }
